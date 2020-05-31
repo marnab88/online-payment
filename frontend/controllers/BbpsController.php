323363221e9fde8f84b6228e816586c4096491df
sandeep <sandeep@krititech.in>
2020-05-31 17:52:08 +0000

    Update BbpsController.php

--- frontend/controllers/BbpsController.php
+++ frontend/controllers/BbpsController.php
@@ -220,7 +220,27 @@ class BbpsController extends Controller {
         echo json_encode($jayParsedAry);
     }
 
- 
+ function get_client_ip()
+{
+    $ipaddress = '';
+    if (getenv('HTTP_CF_CONNECTING_IP'))
+        $ipaddress = getenv('HTTP_CF_CONNECTING_IP');
+    else if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if (getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if (getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if (getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if (getenv('HTTP_FORWARDED'))
+        $ipaddress = getenv('HTTP_FORWARDED');
+    else if (getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
 
     
 }
